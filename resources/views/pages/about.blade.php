53f3c3684f87a1f910468a72ad09cd34835d1a0c
Redolthfi <redolthfi@gmail.com>
2024-09-05 17:31:03 +0700

    create view

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,30 @@
+<x-layout>
+    <section class="bg-white">
+        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <div class="mb-6">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">About Us</h2>
+                <p class="text-gray-500 ">Selamat datang di Ega Selempang, destinasi utama Anda untuk selempang wisuda yang elegan dan penuh makna. Kami berdedikasi untuk memberikan produk berkualitas tinggi yang dibuat dengan perhatian terhadap detail, sehingga setiap momen wisuda Anda menjadi lebih berkesan.</p>
+            </div>
+            <div class="mb-6">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">Visi Kami</h2>
+                <p class="text-gray-500 ">Kami percaya bahwa setiap wisuda adalah perayaan atas kerja keras, pencapaian, dan masa depan yang cerah. Dengan selempang wisuda kami, kami ingin menjadi bagian dari momen spesial Anda, memberikan sentuhan personal dan keindahan pada hari penting tersebut.</p>
+            </div>
+            <div class="mb-6">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">Misi Kami</h2>
+                <p class="text-gray-500 ">Misi kami adalah menyediakan selempang wisuda yang tidak hanya indah tetapi juga sesuai dengan kebutuhan dan keinginan setiap pelanggan. Kami berkomitmen untuk menghadirkan produk dengan kualitas terbaik, desain yang unik, dan layanan pelanggan yang ramah dan profesional.</p>
+            </div>
+            <div class="mb-6">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">Mengapa Memilih Kami?</h2>
+                <p class="text-gray-500 mb-2 "> <span class="text-gray-900 font-semibold">Kualitas Terbaik:</span> Kami menggunakan bahan berkualitas tinggi untuk setiap selempang yang kami produksi, memastikan kenyamanan dan ketahanan.</p>
+                <p class="text-gray-500 mb-2 "> <span class="text-gray-900 font-semibold">Desain Eksklusif:</span> Kami menawarkan berbagai desain selempang, dari yang sederhana hingga yang elegan, dan kami juga menerima pesanan desain khusus sesuai keinginan Anda.</p>
+                <p class="text-gray-500 mb-2 "> <span class="text-gray-900 font-semibold">Pelayanan Terpercaya:</span> Kepuasan pelanggan adalah prioritas kami. Tim kami siap membantu Anda dari proses pemesanan hingga produk tiba di tangan Anda.</p>
+                <p class="text-gray-500 mb-2 "> <span class="text-gray-900 font-semibold">Pengiriman Cepat:</span> Kami mengerti pentingnya waktu, terutama untuk acara spesial seperti wisuda. Oleh karena itu, kami memastikan pengiriman yang cepat dan tepat waktu.</p>
+            </div>
+            <div class="mb-6">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">Hubungi Kami</h2>
+                <p class="text-gray-500 ">Jika Anda memiliki pertanyaan, kebutuhan khusus, atau ingin berbagi cerita wisuda Anda dengan kami, jangan ragu untuk menghubungi tim kami. Kami senang bisa menjadi bagian dari momen berharga Anda!</p>
+            </div>
+            
+        </div>
+      </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,31 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserProfileController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::get('/about', function () {
+    return view('pages/about'); 
+});
+
+Route::get('/faq', function () {
+    return view('pages/faq'); 
+});
+
+Route::get('/contact', function () {
+    return view('pages/contact'); 
+});
+
+Route::get('/products', function () {
+$products = Product::get();
+
+    return view('pages/product', [
+       'products' => $products 
+    ]);
 });
 
 Route::middleware('guest')->group(function () {
@@ -30,14 +51,16 @@ Route::middleware(['auth'])->group(function () {
         });
     });
 
-    Route::prefix('products')->group(function () {
-        Route::get('/', [ProductController::class, 'index'])->name('master.product.index');
-        Route::get('/create', [ProductController::class, 'create'])->name('master.product.create');
-        Route::post('/create', [ProductController::class, 'createPost']);
-
-        Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('master.product.edit');
-        Route::post('/edit/{id}', [ProductController::class, 'editPost']);
-
-        Route::get('/delete/{id}', [ProductController::class, 'delete'])->name('master.product.delete');
+    Route::prefix('master')->group(function(){
+        Route::prefix('products')->group(function () {
+            Route::get('/', [ProductController::class, 'index'])->name('master.product.index');
+            Route::get('/create', [ProductController::class, 'create'])->name('master.product.create');
+            Route::post('/create', [ProductController::class, 'createPost']);
+    
+            Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('master.product.edit');
+            Route::post('/edit/{id}', [ProductController::class, 'editPost']);
+    
+            Route::get('/delete/{id}', [ProductController::class, 'delete'])->name('master.product.delete');
+        });
     });
 });
